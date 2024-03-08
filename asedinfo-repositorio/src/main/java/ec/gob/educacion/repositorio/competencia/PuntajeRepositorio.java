85092c16a885a35fea5a1633ed11218768731e0e
Javier Brito <vjbritoa@hotmail.com>
2024-03-08 16:53:45 -0500

    Ajustes en puntaje y numero de jueces v2.0

--- asedinfo-repositorio/src/main/java/ec/gob/educacion/repositorio/competencia/PuntajeRepositorio.java
+++ asedinfo-repositorio/src/main/java/ec/gob/educacion/repositorio/competencia/PuntajeRepositorio.java
@@ -41,8 +41,9 @@ public interface PuntajeRepositorio extends JpaRepository<Puntaje, Long> {
 			  " select sum(wpu.puntaje) as puntaje, wpu.cod_subcategoria, wpu.cod_instancia, wpu.cod_participante, concat(wpa.first_name, ' ', wpa.last_name) as nombreParticipante, "
 			+ "        wpu.codigo, wpu.cod_modelo_puntaje, wpu.estado " 
 			+ "   from wp_puntaje wpu, wp_participante wpa "
-			+ "  where wpu.cod_modelo_puntaje = 99 and wpu.cod_participante = wpa.codigo " 
+			+ "  where wpu.cod_modelo_puntaje = 99 and wpu.cod_participante = wpa.codigo "
+			+ "    and wpu.cod_subcategoria = :codSubcategoria and wpu.cod_instancia = :codInstancia "
 			+ "  group by wpu.cod_subcategoria, wpu.cod_instancia, wpu.cod_participante " ) 
-	List<Object[]> listarPuntajePorSubcategoriaInstanciaRegSUMA(@Param("codSubcategoria") Long codSubcategoria, @Param("codInstancia") Long codInstancia);;
+	List<Object[]> listarPuntajePorSubcategoriaInstanciaRegSUMA(@Param("codSubcategoria") Long codSubcategoria, @Param("codInstancia") Long codInstancia);
 
 }
